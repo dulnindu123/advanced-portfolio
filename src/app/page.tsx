d50a778a965e034ca06eda34a1140a37275ad57d
Dulnindu Saranga <dulnindusaranga@gmail.com>
2026-05-06 19:17:22 +0530

    Add HelaDry IoT Digital Twin Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import ProjectsSection from "@/components/ProjectsSection";
+import HelaDryDashboard from "@/components/HelaDryDashboard";
 import EducationSection from "@/components/EducationSection";
 import Terminal from "@/components/Terminal";
 import ContactSection from "@/components/ContactSection";
@@ -14,6 +15,7 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
+        <HelaDryDashboard />
         <EducationSection />
         <Terminal />
         <ContactSection />
